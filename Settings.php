783dd76f4beb394303d6b69979aa896973ddeff9
jt222ii <jt222ii@student.lnu.se>
2015-10-27 20:51:46 +0100

    Update Settings.php

--- Settings.php
+++ Settings.php
@@ -1,9 +1,9 @@
 <?php
 
 class Settings{
-	static $mysql_host = "localhost";
-	static $mysql_user = "jt222ii";
-	static $mysql_password = "jt222ii";
-	static $mysql_database = "register";
-	static $salt = "mmsalt";
+	static $mysql_host = "";
+	static $mysql_user = "";
+	static $mysql_password = "";
+	static $mysql_database = "";
+	static $salt = "";
 }
